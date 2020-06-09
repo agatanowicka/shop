efa13489aa2c9863f217c9e81b438a79c36059ae
Agata Nowicka <nowicka.agata2201@gmail.com>
2020-06-09 12:15:29 +0200

    separate all routes in allRoutes function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React, { Component, Fragment } from "react";
 import './App.css';
-import { Route, Switch, Redirect, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import Header from "./Commponents/Header";
 import Footer from "./Commponents/Footer";
-import PageOneProduct from "./Commponents/PageOneProduct/PageOneProduct";
-import Catalog from "./Commponents/Catalog/Catalog";
-import HomePage from "./Commponents/Home/HomePage";
-import LoginPage from "./Commponents/Authorization/Login/LoginPage";
-import SignupPage from "./Commponents/Authorization/Signup/SignupPage";
-import CardMenuForm from "./Commponents/AdministratorPages/CardMenuForm";
-import AddNewProductForm from './Commponents/AdministratorPages/AddNewProductForm';
-import SchoppingBasketPage from './Commponents/SchoppingBasket/SchoppingBasketPage';
-import Logout from './Commponents/Authorization/Logout/Logout';
+import allRoutes from './allRoutes';
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -25,203 +18,13 @@ class App extends Component {
     }
   }
   render() {
-    let routes = (
-      <Switch>
-        <Route
-          path="/"
-          exact
-          render={props => (
-            <HomePage
-              token={this.state.token}
-            />
-          )}
-        />
-        <Route
-          path="/Catalog"
-          exact
-          render={props => (
-            <Catalog
-              {...props}
-              token={this.state.token}
-            />
-          )}
-        />
-        <Route
-          path="/Catalog/:type"
-          exact
-          location={this.props.location}
-          key={this.props.location.key}
-          render={props => (
-            <Catalog
-              {...props}
-              token={this.state.token}
-              key={this.props.location.key}
-            />
-          )}
-        />
-        <Route
-          path="/Product/:id"
-          exact
-          render={props => (
-            <PageOneProduct
-              {...props}
-              token={this.state.token}
-            />
-          )}
-        />
-        <Route
-          path="/SchoppingBasket"
-          exact
-          render={props => (
-            <SchoppingBasketPage
-              {...props}
-              token={this.state.token}
-              isAuth={this.state.isAuth}
-            />
-          )}
-        />
-        <Route
-          path="/Login"
-          exact
-          render={props => (
-            <LoginPage
-              {...props}
-            />
-          )}
-        />
-        <Route
-          path="/Logout"
-          exact
-          render={props => (
-            <Logout
-              {...props}
-            />
-          )}
-        />
-        <Route
-          path="/Signup"
-          exact
-          render={props => (
-            <SignupPage
-              {...props}
-            />
-          )}
-        />
-        <Redirect to="/" />
-      </Switch>
-    );
-    if (this.state.isAuth && this.state.isAministrator) {
-      routes = (
-        <Switch>
-             <Route
-          path="/"
-          exact
-          render={props => (
-            <HomePage
-              token={this.state.token}
-            />
-          )}
-        />
-        <Route
-          path="/Catalog"
-          exact
-          render={props => (
-            <Catalog
-              {...props}
-              token={this.state.token}
-            />
-          )}
-        />
-        <Route
-          path="/Catalog/:type"
-          exact
-          location={this.props.location}
-          key={this.props.location.key}
-          render={props => (
-            <Catalog
-              {...props}
-              token={this.state.token}
-              key={this.props.location.key}
-            />
-          )}
-        />
-        <Route
-          path="/Product/:id"
-          exact
-          render={props => (
-            <PageOneProduct
-              {...props}
-              token={this.state.token}
-            />
-          )}
-        />
-        <Route
-          path="/SchoppingBasket"
-          exact
-          render={props => (
-            <SchoppingBasketPage
-              {...props}
-              token={this.state.token}
-              isAuth={this.state.isAuth}
-            />
-          )}
-        />
-        <Route
-          path="/Login"
-          exact
-          render={props => (
-            <LoginPage
-              {...props}
-            />
-          )}
-        />
-        <Route
-          path="/Logout"
-          exact
-          render={props => (
-            <Logout
-              {...props}
-            />
-          )}
-        />
-        <Route
-          path="/Signup"
-          exact
-          render={props => (
-            <SignupPage
-              {...props}
-            />
-          )}
-        />
-          <Route
-            path="/CardMenuForm"
-            exact
-            render={props => (
-              <CardMenuForm
-                token={this.state.token}
-              />
-            )}
-          />
-          <Route
-            path="/AddNewProductForm"
-            render={props => (
-              <AddNewProductForm
-                {...props}
-                token={this.state.token}
-              />
-            )}
-          />
-          <Redirect to="/" />
-        </Switch>
-      );
-    }
     return (
       <Fragment>
         <Header
           isAuth={this.state.isAuth}
           isAministrator={this.state.isAministrator}
         />
-        {routes}
+        {allRoutes(this.state)}
         <Footer />
       </Fragment>
     );
---
+++ src/allRoutes.js
@@ -0,0 +1,207 @@
+import React from "react";
+import { Route, Switch, Redirect } from 'react-router-dom';
+import PageOneProduct from "./Commponents/PageOneProduct/PageOneProduct";
+import Catalog from "./Commponents/Catalog/Catalog";
+import HomePage from "./Commponents/Home/HomePage";
+import LoginPage from "./Commponents/Authorization/Login/LoginPage";
+import SignupPage from "./Commponents/Authorization/Signup/SignupPage";
+import CardMenuForm from "./Commponents/AdministratorPages/CardMenuForm";
+import AddNewProductForm from './Commponents/AdministratorPages/AddNewProductForm';
+import SchoppingBasketPage from './Commponents/SchoppingBasket/SchoppingBasketPage';
+import Logout from './Commponents/Authorization/Logout/Logout';
+
+function allRoutes(props) {
+
+    let routes = (
+        <Switch>
+            <Route
+                path="/"
+                exact
+                render={props => (
+                    <HomePage
+                        token={props.token}
+                    />
+                )}
+            />
+            <Route
+                path="/Catalog"
+                exact
+                render={props => (
+                    <Catalog
+                        {...props}
+                        token={props.token}
+                    />
+                )}
+            />
+            <Route
+                path="/Catalog/:type"
+                exact
+                location={props.location}
+                key={props.location}
+                render={props => (
+                    <Catalog
+                        {...props}
+                        token={props.token}
+                        key={props.location.key}
+                    />
+                )}
+            />
+            <Route
+                path="/Product/:id"
+                exact
+                render={props => (
+                    <PageOneProduct
+                        {...props}
+                        token={props.token}
+                    />
+                )}
+            />
+            <Route
+                path="/SchoppingBasket"
+                exact
+                render={props => (
+                    <SchoppingBasketPage
+                        {...props}
+                        token={props.token}
+                        isAuth={props.isAuth}
+                    />
+                )}
+            />
+            <Route
+                path="/Login"
+                exact
+                render={props => (
+                    <LoginPage
+                        {...props}
+                    />
+                )}
+            />
+            <Route
+                path="/Logout"
+                exact
+                render={props => (
+                    <Logout
+                        {...props}
+                    />
+                )}
+            />
+            <Route
+                path="/Signup"
+                exact
+                render={props => (
+                    <SignupPage
+                        {...props}
+                    />
+                )}
+            />
+            <Redirect to="/" />
+        </Switch>
+    );
+    if (props.isAuth && props.isAministrator) {
+        routes = (
+            <Switch>
+                <Route
+                    path="/"
+                    exact
+                    render={props => (
+                        <HomePage
+                            token={props.token}
+                        />
+                    )}
+                />
+                <Route
+                    path="/Catalog"
+                    exact
+                    render={props => (
+                        <Catalog
+                            {...props}
+                            token={props.token}
+                        />
+                    )}
+                />
+                <Route
+                    path="/Catalog/:type"
+                    exact
+                    location={props.location}
+                    key={props.location}
+                    render={props => (
+                        <Catalog
+                            {...props}
+                            token={props.token}
+                            key={props.location}
+                        />
+                    )}
+                />
+                <Route
+                    path="/Product/:id"
+                    exact
+                    render={props => (
+                        <PageOneProduct
+                            {...props}
+                            token={props.token}
+                        />
+                    )}
+                />
+                <Route
+                    path="/SchoppingBasket"
+                    exact
+                    render={props => (
+                        <SchoppingBasketPage
+                            {...props}
+                            token={props.token}
+                            isAuth={props.isAuth}
+                        />
+                    )}
+                />
+                <Route
+                    path="/Login"
+                    exact
+                    render={props => (
+                        <LoginPage
+                            {...props}
+                        />
+                    )}
+                />
+                <Route
+                    path="/Logout"
+                    exact
+                    render={props => (
+                        <Logout
+                            {...props}
+                        />
+                    )}
+                />
+                <Route
+                    path="/Signup"
+                    exact
+                    render={props => (
+                        <SignupPage
+                            {...props}
+                        />
+                    )}
+                />
+                <Route
+                    path="/CardMenuForm"
+                    exact
+                    render={props => (
+                        <CardMenuForm
+                            token={props.token}
+                        />
+                    )}
+                />
+                <Route
+                    path="/AddNewProductForm"
+                    render={props => (
+                        <AddNewProductForm
+                            {...props}
+                            token={props.token}
+                        />
+                    )}
+                />
+                <Redirect to="/" />
+            </Switch>
+        );
+    }
+    return routes
+}
+export default allRoutes
