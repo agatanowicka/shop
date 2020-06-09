ac856baf36c5096373ecae29dc0a1cc69aa85b1f
Agata Nowicka <nowicka.agata2201@gmail.com>
2020-06-09 12:07:42 +0200

    add to the header the possibility to choose the type of clothes from the collection

--- src/Commponents/Header.js
+++ src/Commponents/Header.js
@@ -2,29 +2,107 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from 'react-bootstrap/Nav';
 import { FaShoppingCart } from "react-icons/fa";
+import Navbar from 'react-bootstrap/Navbar';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+import getClothesTypes from './GetClothesTypes';
+
 class Header extends Component {
-    render(){
-    return (
-        <Nav
-            defaultActiveKey="/home" as="ul"
-            className="header"
-        >
-            <Nav.Item>
-                <Nav.Link className="linkItem" href="/">Home</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link className="linkItem" href="/Catalog" > Colection</Nav.Link>
-            </Nav.Item>
+    constructor(props) {
+        super(props);
+        this.getData();
+        this.state = {
+            types: []
+        }
+    }
+    getData = async () => {
+        const types = await getClothesTypes();
+        this.setState({
+            types
+        })
+    }
+    render() {
+        return (
+            <Navbar
+                fixed="top"
+                collapseOnSelect
+                expand="lg"
+                className="header justify-content-between"
+            >
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto" inline>
+                        <Nav.Link
+                            href="/"
+                            className='linkItem'
+                            style={{ color: "white" }}>
+                            Home
+                        </Nav.Link>
+                        <OverlayTrigger
+                            trigger="click"
+                            placement="bottom"
+                            overlay={
+                                <Tooltip >
+                                    {this.state.types.map((type) => {
+                                        const path = `/Catalog/${type.type}`
+                                        return (
+                                            <Nav.Link
+                                                href={path}
+                                                style={{ color: 'white' }}>
+                                                {type.type}
+                                            </Nav.Link>
+                                        )
+                                    })}
+                                </Tooltip>
+                            }
+                        >
+                            <Nav.Link
+                                className='linkItem'
+                                style={{ color: "white" }}>
+                                Colection
+                                  </Nav.Link>
+                        </OverlayTrigger>
 
-            {!this.props.isAuth ? <Nav.Item><Nav.Link className="linkItem" href="/Login">LogIn</Nav.Link></Nav.Item> : <Nav.Item ><Nav.Link className="linkItem" href='/Logout'>Logout</Nav.Link></Nav.Item>}
-
-            {this.props.isAuth && this.props.isAministrator ? <Nav.Item><Nav.Link className="linkItem" href="/AddNewProductForm">Add product</Nav.Link></Nav.Item> : ''}
-            {this.props.isAuth && this.props.isAministrator ? <Nav.Item><Nav.Link className="linkItem" href="/CardMenuForm">Add card menu</Nav.Link></Nav.Item> : ''}
-       
-            <Nav.Item>
-                <Nav.Link className="linkItem" href="/SchoppingBasket" ><FaShoppingCart /> </Nav.Link>
-            </Nav.Item>
-        </Nav>
-    )}
+                        {this.props.isAuth && this.props.isAministrator ?
+                            <Nav.Link
+                                href="/AddNewProductForm"
+                                className='linkItem'
+                                style={{ color: "white" }}>
+                                Add product
+                             </Nav.Link>
+                            : ''}
+                        {this.props.isAuth && this.props.isAministrator ?
+                            <Nav.Link
+                                href="/CardMenuForm"
+                                className='linkItem'
+                                style={{ color: "white" }}>
+                                Add card menu
+                             </Nav.Link>
+                            : ''}
+                    </Nav> <Nav inline>
+                        <Nav.Link
+                            href="/SchoppingBasket"
+                            className='linkItem'
+                            style={{ color: "white" }}>
+                            <FaShoppingCart />
+                        </Nav.Link>
+                        {!this.props.isAuth ?
+                            <Nav.Link
+                                href="/Login"
+                                className='linkItem'
+                                style={{ color: "white" }}>
+                                LogIn
+                              </Nav.Link>
+                            : <Nav.Link
+                                className='linkItem'
+                                href='/Logout'
+                                style={{ color: "white" }}>
+                                Logout
+                            </Nav.Link>}
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
 }
 export default Header
