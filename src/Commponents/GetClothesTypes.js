7f000cf0a6fc3ca077bd8691065aa410960ff3b3
Agata Nowicka <nowicka.agata2201@gmail.com>
2020-06-09 12:13:43 +0200

    separate getClothesTypes function

---
+++ src/Commponents/GetClothesTypes.js
@@ -0,0 +1,16 @@
+
+export default async function getClothesTypes  () {
+
+        return fetch('http://localhost:8080/cardMenu/', { method: 'GET' })
+        .then(res => {
+            if (res.status !== 200) {
+                return alert('Failed to fetch status')
+            }
+            return res.json();
+        })
+        .catch(err => {
+            console.log(err);
+            return [];
+        })
+
+}
--- src/Commponents/Home/CardMenu.js
+++ src/Commponents/Home/CardMenu.js
@@ -4,7 +4,9 @@ import Card from 'react-bootstrap/Card'
 import { Redirect } from 'react-router-dom';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import Col from 'react-bootstrap/Col';
+import getClothesTypes from '../GetClothesTypes';
+
 class CardMenu extends Component {
     constructor(props) {
         super(props);
@@ -15,23 +17,13 @@ class CardMenu extends Component {
         }
     }
 
-    getData = () => {
-        fetch('http://localhost:8080/cardMenu/', { method: 'GET' })
-            .then(res => {
-                if (res.status !== 200) {
-                    return alert('Failed to fetch status')
-                }
-                return res.json();
-            })
-            .then(resData => {
-                this.setState({
-                    cards: resData
-                })
-            })
-            .catch(err => {
-                console.log(err);
-            })
+    getData = async () => {
+        const cards = await getClothesTypes();
+        this.setState({
+            cards: cards
+        })
     }
+
     redirectPage = (type) => {
         this.setState({
             redirect: type
@@ -51,9 +43,19 @@ class CardMenu extends Component {
                             return (
                                 <Col xs={12} s={12} md={6} lg={4}>
                                     <a onClick={() => this.redirectPage(card.type)}>
-                                        <Card style={{ backgroundImage: this.state.image, width: '300px' }} className='cardWithTypesClothes'>
+                                        <Card
+                                            style={{ width: '300px' }}
+                                            className='cardWithTypesClothes'
+                                        >
                                             <Card.Body >
-                                                <Card.Title className="cardHomeTitle"><h1>{card.title}</h1></Card.Title>
+                                                <Card.Title
+                                                    className="cardHomeTitle">
+                                                    <h1>{card.title}</h1>
+                                                </Card.Title>
+                                                <Card.Img
+                                                    variant="bottom"
+                                                    src={card.image}
+                                                    height='250px' />
                                             </Card.Body>
                                         </Card>
                                     </a>
